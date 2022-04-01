2cadf1c5bde0147d6dc839d024c7a44f52847d92
Your Name <you@example.com>
2022-04-01 12:32:07 +0100

    final commit

--- cookie.php
+++ cookie.php
@@ -4,12 +4,15 @@
 
 if (isset($_COOKIE["user"]) )
 {?>
-    <script> alert( "vote already done" )</script>
+    <script>
+   alert( 'vous avez deja voté  <?=$_COOKIE["user"] ?> ')
+       </script>
 <?php }
     else
     {$cookie_name = "user";
         $cookie_value = $_POST["degree"];
         setcookie($cookie_name, $cookie_value, time() + (2*60), "/"); // 86400 = 1 day
+
         ?>
         <script> alert( "vote enregistré" )</script>
         <?php
